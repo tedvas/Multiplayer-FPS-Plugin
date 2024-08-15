db0ffa3ea12a8550d91dadf12dc23850d62327d5
vster <vasanton2112@gmail.com>
2024-08-14 23:26:21 -0400

    fixed issue with burst on client
    burst would fire one more shot than it should on the client

--- PluginExample/Plugins/MultiplayerFPS/Source/MultiplayerFPS/Private/MultiplayerPlayerController.cpp
+++ PluginExample/Plugins/MultiplayerFPS/Source/MultiplayerFPS/Private/MultiplayerPlayerController.cpp
@@ -1,5 +1,4 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-// the reason you'll see weird parenthesis on if statements is for linux compatibility
 
 #include "MultiplayerPlayerController.h"
 #include "Net/UnrealNetwork.h"
